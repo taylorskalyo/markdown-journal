9248d72bfef951f652c121b6c39ec2123704023d
taylorskalyo <taylorskalyo@gmail.com>
2019-10-02 16:19:19 -0400

    Add "timeline" command

---
+++ commands/timeline.go
@@ -0,0 +1,50 @@
+package commands
+
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/taylorskalyo/markdown-journal/ctags"
+	"github.com/taylorskalyo/markdown-journal/ctags/parser"
+	"github.com/taylorskalyo/markdown-journal/journal"
+)
+
+func init() {
+	application.AddCommand(timelineCommand)
+
+	recurseDesc := `recurse into directories`
+	timelineCommand.Flags().BoolVarP(&recurse, "recurse", "R", false, recurseDesc)
+}
+
+var timelineCommand = &cobra.Command{
+	Use:   "timeline [files]",
+	Short: "Display a timeline view",
+	Long:  `This command displays a timeline view of journal entries.`,
+	Args:  cobra.ArbitraryArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		var filenames []string
+		var tagLines []ctags.TagLine
+		var err error
+
+		if len(args) > 0 {
+			filenames, err = journal.Files(args, recurse)
+		} else {
+			filenames, err = journal.Files([]string{"."}, recurse)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		p := parser.NewEntryParser()
+		for _, filename := range filenames {
+			lines, err := p.Parse(filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tagLines = append(tagLines, lines...)
+		}
+
+		journal.WriteTimeline(tagLines, os.Stdout)
+	},
+}
--- journal/journal.go
+++ journal/journal.go
@@ -1,10 +1,21 @@
 package journal
 
 import (
+	"fmt"
+	"io"
 	"os"
+	"path"
 	"path/filepath"
 	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/taylorskalyo/markdown-journal/ctags"
 )
+
+const dateFormat = "2006-01-02"
 
 // Files finds journal entry files. It walks each given path checking for ones
 // that look like journal entries. It returns a list of the entries it finds.
@@ -37,4 +48,87 @@ func Files(paths []string, recurse bool) (entries []string, err error) {
 func isJournalFile(file string) bool {
 	re := regexp.MustCompile(`\d{4}-\d{2}-\d{2}(-.*)?\.md`)
 	return re.MatchString(file)
+}
+
+// WriteTimeline generates a timeline view of entries, using ctags, and writes
+// the result to a writer.
+func WriteTimeline(tagLines []ctags.TagLine, w io.Writer) error {
+	sort.Slice(tagLines, func(i, j int) bool {
+		return tagLines[i].TagFile < tagLines[j].TagFile
+	})
+
+	entries := make(map[string][]ctags.TagLine)
+	for _, tagLine := range tagLines {
+		entryTags, ok := entries[tagLine.TagFile]
+		if !ok {
+			entryTags = []ctags.TagLine{}
+		}
+		entries[tagLine.TagFile] = append(entryTags, tagLine)
+	}
+
+	var entryFiles []string
+	for k := range entries {
+		entryFiles = append(entryFiles, k)
+		sort.Slice(entries[k], func(i, j int) bool {
+			var li, lj int
+			var err error
+			for _, tf := range entries[k][i].TagFields {
+				if tf.Name == "line" {
+					li, err = strconv.Atoi(tf.Value)
+					if err != nil {
+						continue
+					}
+					break
+				}
+			}
+			for _, tf := range entries[k][j].TagFields {
+				if tf.Name == "line" {
+					lj, err = strconv.Atoi(tf.Value)
+					if err != nil {
+						continue
+					}
+					break
+				}
+			}
+			return li > lj
+		})
+	}
+	sort.Strings(entryFiles)
+
+	var year int
+	var month time.Month
+	for _, entryFile := range entryFiles {
+		var entryTitle string
+
+		for _, entryTag := range entries[entryFile] {
+			for _, field := range entryTag.TagFields {
+				if field.Name == "kind" && field.Value == "heading" {
+					entryTitle = entryTag.TagName
+				}
+			}
+		}
+		entryName := strings.TrimSuffix(path.Base(entryFile), path.Ext(entryFile))
+		entryDate, err := time.Parse(dateFormat, entryName)
+		if err != nil {
+			return err
+		}
+
+		if year != entryDate.Year() {
+			year = entryDate.Year()
+			fmt.Fprintf(w, "\n# %d\n", year)
+		}
+
+		if month != entryDate.Month() {
+			month = entryDate.Month()
+			fmt.Fprintf(w, "\n## %s\n", month)
+		}
+
+		if entryTitle != "" {
+			fmt.Fprintf(w, "* [%d %s](%s) - %s\n", entryDate.Day(), entryDate.Weekday(), entryFile, entryTitle)
+		} else {
+			fmt.Fprintf(w, "* [%d %s](%s)\n", entryDate.Day(), entryDate.Weekday(), entryFile)
+		}
+	}
+
+	return nil
 }
