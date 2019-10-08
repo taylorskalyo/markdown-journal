107f547f705458e2bb5d85ee50e46dc6cf458719
taylorskalyo <taylorskalyo@gmail.com>
2019-10-08 16:22:35 -0400

    Add flag to generate timeline from cached ctags info
    When this flag is enabled the timeline command reads ctags from a file
    instead of generating them on the fly.
    
    This could be especially useful for users with very large journal
    entries.

--- commands/timeline.go
+++ commands/timeline.go
@@ -12,6 +12,9 @@ import (
 func init() {
 	application.AddCommand(timelineCommand)
 
+	tagfileDesc := `read entry info from specified tags file; "-" reads tags from stdin`
+	timelineCommand.Flags().StringVarP(&tagfileName, "tagfile", "f", "", tagfileDesc)
+
 	recurseDesc := `recurse into directories`
 	timelineCommand.Flags().BoolVarP(&recurse, "recurse", "R", false, recurseDesc)
 }
@@ -26,24 +29,57 @@ var timelineCommand = &cobra.Command{
 		var tagLines []ctags.TagLine
 		var err error
 
-		if len(args) > 0 {
-			filenames, err = journal.Files(args, recurse)
+		if tagfileName == "" {
+
+			if len(args) > 0 {
+				filenames, err = journal.Files(args, recurse)
+			} else {
+				filenames, err = journal.Files([]string{"."}, recurse)
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			tagLines, err = generateCtags(filenames)
 		} else {
-			filenames, err = journal.Files([]string{"."}, recurse)
+
+			tagLines, err = readCtags(tagfileName)
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		p := journal.NewEntryParser()
-		for _, filename := range filenames {
-			lines, err := p.Parse(filename)
-			if err != nil {
-				log.Fatal(err)
-			}
-			tagLines = append(tagLines, lines...)
-		}
-
 		journal.WriteTimeline(tagLines, os.Stdout)
 	},
+}
+
+func readCtags(tagfileName string) (tagLines []ctags.TagLine, err error) {
+	var tagfile *os.File
+
+	if tagfileName == "-" {
+		tagfile = os.Stdin
+	} else {
+		tagfile, err = os.Open(tagfileName)
+		if err != nil {
+			return tagLines, err
+		}
+	}
+
+	r := ctags.NewReader(tagfile)
+	tagLines = r.ReadAll()
+
+	return tagLines, err
+}
+
+func generateCtags(filenames []string) (tagLines []ctags.TagLine, err error) {
+	p := journal.NewEntryParser()
+	for _, filename := range filenames {
+		lines, err := p.Parse(filename)
+		if err != nil {
+			return tagLines, err
+		}
+		tagLines = append(tagLines, lines...)
+	}
+
+	return tagLines, err
 }
