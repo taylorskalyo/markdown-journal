7282796698b3abba0b1097dfb039fde664aa364b
taylorskalyo <taylorskalyo@gmail.com>
2019-10-15 16:40:22 -0400

    Test WriteLabels method

---
+++ journal/labels_test.go
@@ -0,0 +1,75 @@
+package journal
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/taylorskalyo/markdown-journal/ctags"
+)
+
+func TestWriteLabels(t *testing.T) {
+	format := `
+============= case %s ================
+Ctags Input:
+-----------
+%v
+Expected Output:
+----------
+%v
+Actual Output:
+----------
+%v
+`
+
+	cases := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			`basics`,
+			`
+03 Tuesday	diary/2006-01-03.md	1;"	kind:heading	line:1
+recipe	diary/2006-01-03.md	5;"	kind:label	line:5
+30 Friday	diary/2007-11-30.md	1;"	kind:heading	line:1
+recipe	diary/2007-11-30.md	3;"	kind:label	line:3
+Ingredients	diary/2007-11-30.md	6;"	kind:heading	line:6
+groceries	diary/2007-11-30.md	14;"	kind:label	line:14
+groceries	diary/2007-11-30.md	16;"	kind:label	line:16
+			`,
+			`
+# groceries
+* [Ingredients](diary/2007-11-30.md:16)
+* [Ingredients](diary/2007-11-30.md:14)
+
+# recipe
+* [30 Friday](diary/2007-11-30.md:3)
+* [03 Tuesday](diary/2006-01-03.md:5)
+			`,
+		},
+		{
+			`no heading`,
+			`
+recipe	diary/2006-01-04.md	5;"	kind:label	line:5
+			`,
+			`
+# recipe
+* [diary/2006-01-04.md:5](diary/2006-01-04.md:5)
+			`,
+		},
+	}
+
+	for _, tc := range cases {
+		var b bytes.Buffer
+
+		r := ctags.NewReader(strings.NewReader(tc.input))
+		j := NewJournal(r.ReadAll())
+		j.WriteLabels(&b)
+		actual := strings.TrimSpace(b.String())
+		expected := strings.TrimSpace(tc.expected)
+		if actual != expected {
+			t.Errorf(format, tc.name, tc.input, expected, actual)
+		}
+	}
+}
